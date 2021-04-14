9fbe4449fd08580c6b8613cd29198135519476b2
CopyrightAtCLLS <939085525@qq.com>
2021-04-14 19:47:33 +0800

    update comments

--- src/main/java/serializable/susceptible2change/User.java
+++ src/main/java/serializable/susceptible2change/User.java
@@ -5,6 +5,8 @@ import java.io.*;
 /**
  * 序列化后，添加成员变量
  * 再反序列化，就会抛出java.io.InvalidClassException
+ *
+ * 即使只是修改变量名也会导致hash变化
  */
 public class User implements Serializable {
     public String name;
