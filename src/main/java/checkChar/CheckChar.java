824b035aa9a196184c72402d35eaf7d9a4932bf0
CopyrightAtCLLS <939085525@qq.com>
2021-05-09 10:35:41 +0800

    shallowly delve into char

---
+++ src/main/java/checkChar/CheckChar.java
@@ -0,0 +1,11 @@
+package checkChar;
+
+public class CheckChar {
+    public static void main(String[] args) {
+        System.out.println((char) 33);
+        System.out.println((char) 65);
+        System.out.println((char) 126);
+        System.out.println((int) '你');
+        System.out.println((char) 20320);
+    }
+}
